487e88e77dce480d4046258fc02aaa8fdd8b7783
laszlo-caballero <laszlocaballero@gmail.com>
2025-03-04 23:04:29 -0500

    Docker :DDDDDDDDDDDDDDDDDDD junto al servidor del front

--- client/main.cjs
+++ client/main.cjs
@@ -1,5 +1,5 @@
-import path from "path";
-import express from "express";
+const path = require("path");
+const express = require("express");
 
 const app = express();
 
@@ -7,7 +7,7 @@ const PORT = 8000;
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.get("*", (_, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
