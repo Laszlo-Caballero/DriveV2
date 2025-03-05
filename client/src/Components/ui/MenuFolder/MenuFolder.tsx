963fdb1d43c1f4e1c8c61e1140f7aea61c17f27a
laszlo-caballero <laszlocaballero@gmail.com>
2025-03-04 23:43:12 -0500

    Agregar funcionalidad para cerrar el menú al hacer clic fuera de él en el componente MenuFolder

--- client/src/Components/ui/MenuFolder/MenuFolder.tsx
+++ client/src/Components/ui/MenuFolder/MenuFolder.tsx
@@ -1,21 +1,25 @@
 import { TrashIcon } from "@/assets/icons/TrashIcon";
 import Option from "../MenuFile/Option";
+import { useCloseDiv } from "@/hooks/useCloseDiv";
 
 interface Props {
   closeFunction?: () => void;
   openDeleteModal?: () => void;
 }
 
-export default function MenuFolder({ openDeleteModal }: Props) {
+export default function MenuFolder({ openDeleteModal, closeFunction }: Props) {
+  const ref = useCloseDiv({ closeFunction });
+
   return (
-    <>
-      <div className="absolute right-0 top-full z-10 flex -translate-y-3 flex-col rounded-xl bg-blue-zodiac-100 font-monserrat text-xl">
-        <Option
-          icon={<TrashIcon />}
-          label="Eliminar"
-          onClick={() => openDeleteModal?.()}
-        />
-      </div>
-    </>
+    <div
+      className="absolute right-0 top-full z-10 flex -translate-y-3 flex-col rounded-xl bg-blue-zodiac-100 font-monserrat text-xl"
+      ref={ref}
+    >
+      <Option
+        icon={<TrashIcon />}
+        label="Eliminar"
+        onClick={() => openDeleteModal?.()}
+      />
+    </div>
   );
 }
